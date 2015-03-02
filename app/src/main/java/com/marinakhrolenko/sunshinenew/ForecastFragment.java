0e82f7199e8d7e5017f00a6ad9418ab863e7e9d0
MarinaKhrolenko <rubka-m@mail.ru>
2015-03-02 23:21:44 +0200

    lesson 3

--- app/src/main/java/com/marinakhrolenko/sunshinenew/ForecastFragment.java
+++ app/src/main/java/com/marinakhrolenko/sunshinenew/ForecastFragment.java
@@ -3,9 +3,12 @@ package com.marinakhrolenko.sunshinenew;
 /**
  * Created by Администратор on 24.02.2015.
  */
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
 import android.text.format.Time;
 import android.util.Log;
@@ -15,8 +18,10 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -31,6 +36,7 @@ import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -59,39 +65,50 @@ public class ForecastFragment extends Fragment {
     public boolean onOptionsItemSelected(MenuItem item) {
         int id=item.getItemId();
         if(id==R.id.action_refresh){
-            FetchWeatherTask weatherTask=new FetchWeatherTask();
-            weatherTask.execute("703448");
+            updateWeather();
             return true;
         }
         return super.onOptionsItemSelected(item);
     }
 
+    private void updateWeather(){
+            FetchWeatherTask weatherTask=new FetchWeatherTask();
+            SharedPreferences prefs= PreferenceManager.getDefaultSharedPreferences(getActivity());
+            String location=prefs.getString(getString(R.string.pref_location_key),
+                    getString(R.string.pref_location_default));
+            weatherTask.execute(location);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        updateWeather();
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-
-        String[] forecastArray={
-                "Today - Sunday - 88/63",
-                "Tomorrow - Foggy - 70/40",
-                "Weds - Cloudy - 72/63",
-                "Thurs - Asteroids - 75/65",
-                "Fri - Heavy Rain - 65/56",
-                "Sat - HELP TRAPPED IN WEATHERSTATION - 60/51",
-                "Sun - Sunny - 80/68"
-        };
-
-        List<String> weatherForecast=new ArrayList<String>(Arrays.asList(forecastArray));
 
         mForecastAdapter=new ArrayAdapter<String>(
                 getActivity(),
                 R.layout.list_item_forecast,
                 R.id.list_item_forecast_textview,
-                weatherForecast);
+                new ArrayList<String>());
 
-        ListView listView=(ListView) rootView.findViewById(
-                R.id.listview_forecast);
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        ListView listView=(ListView) rootView.findViewById(R.id.listview_forecast);
         listView.setAdapter(mForecastAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String forecast=mForecastAdapter.getItem(position);
+                Intent intent=new Intent(getActivity(),DetailActivity.class)
+                        .putExtra(Intent.EXTRA_TEXT,forecast);
+                startActivity(intent);
+            }
+        });
 
         return rootView;
     }
@@ -103,14 +120,28 @@ public class ForecastFragment extends Fragment {
         private String getReadableDateString(long time) {
             // Because the API returns a unix timestamp (measured in seconds),
             // it must be converted to milliseconds in order to be converted to valid date.
-            SimpleDateFormat shortenedDateFormat = new SimpleDateFormat("EEE MMM dd");
-            return shortenedDateFormat.format(time);
+            Date date=new Date(time*1000);
+            SimpleDateFormat format = new SimpleDateFormat("E, MMM dd");
+            return format.format(date).toString();
         }
 
         /**
          * Prepare the weather high/lows for presentation.
          */
         private String formatHighLows(double high, double low) {
+
+           SharedPreferences sharedPrefs=
+                   PreferenceManager.getDefaultSharedPreferences(getActivity());
+            String unitType=sharedPrefs.getString(getString(R.string.pref_units_key),
+                    getString(R.string.pref_units_metric));
+
+            if(unitType.equals(getString(R.string.pref_units_imperial))){
+                high=(high*1.8)+32;
+                low=(low*1.8)+32;
+            } else if(unitType.equals(getString(R.string.pref_units_metric))){
+                Log.d(LOG_TAG,"Unit type not found: "+unitType);
+            }
+
             // For presentation, assume the user doesn't care about tenths of a degree.
             long roundedHigh = Math.round(high);
             long roundedLow = Math.round(low);
--- app/src/main/java/com/marinakhrolenko/sunshinenew/MainActivity.java
+++ app/src/main/java/com/marinakhrolenko/sunshinenew/MainActivity.java
@@ -1,12 +1,20 @@
 package com.marinakhrolenko.sunshinenew;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
+import android.preference.PreferenceManager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    private final String LOG_TAG = MainActivity.class.getSimpleName();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +31,7 @@ public class MainActivity extends ActionBarActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-//        getMenuInflater().inflate(R.menu.main, menu);
+       getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
@@ -36,10 +44,38 @@ public class MainActivity extends ActionBarActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            Intent intent=new Intent(this,SettingsActivity.class);
+            startActivity(intent);
+            return true;
+        }
+
+        if (id == R.id.action_map) {
+            openPreferredLocationMap();
             return true;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void openPreferredLocationMap(){
+        SharedPreferences sharedPrefs=
+                PreferenceManager.getDefaultSharedPreferences(this);
+        String location=sharedPrefs.getString(getString(R.string.pref_location_key),
+                                    getString(R.string.pref_location_default));
+
+        Uri geoLocation = Uri.parse("geo:0,0?").buildUpon()
+                       .appendQueryParameter("id", location)
+                       .build();
+
+        Intent intent=new Intent(Intent.ACTION_VIEW);
+        intent.setData(geoLocation);
+
+        if (intent.resolveActivity(getPackageManager()) != null) {
+             startActivity(intent);
+            } else {
+            Log.d(LOG_TAG, "Couldn't call " + location + ", no receiving apps installed!");
+           }
+
     }
 
 
